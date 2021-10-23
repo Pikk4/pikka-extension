abf84b97baea4fa3b24a19dbbedbdcf505b92062
Jose Vargas <ruedagato@gmail.com>
2021-10-23 03:58:15 -0500

    add firebase

--- src/background.js
+++ src/background.js
@@ -1,15 +1,22 @@
-console.log("init back ground");
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
 
-let color = "#3aa757";
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %cgreen", `color: ${color}`);
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyB0Z4GMb_mHSL582D3dU0AFE0wsfaK8zb8",
+  authDomain: "pika-4af18.firebaseapp.com",
+  databaseURL: "https://pika-4af18.firebaseio.com",
+  projectId: "pika-4af18",
+  storageBucket: "pika-4af18.appspot.com",
+  messagingSenderId: "197084647841",
+  appId: "1:197084647841:web:167611e1542196c3543be0",
+  measurementId: "G-L0BSEVKF74",
+};
 
-chrome.storage.onChanged.addListener((changes, namespace) => {
-  console.log("change", changes, namespace);
-});
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+console.log(db);
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log(message);
